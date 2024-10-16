b0f8ffa551c81d915ba5ab927d5d3f4c9006d230
Lojitao <Lojitao@outlook.com>
2024-10-16 20:09:20 +0800

    清除不需要的檔案

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+//項目入口，從這裡開始運行
+
+//React必要的核心包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//導入項目組件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//把APP根結點渲染到id為root的dom節點上
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
