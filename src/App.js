1bf5cecab143a8a6af6b3d4edec2e4731c098879
Lojitao <Lojitao@outlook.com>
2024-10-19 17:00:07 +0800

    新增組件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 //項目的根組件
 //APP->index.js->public/index.html(root)
+import { useEffect,useState } from 'react'
+
+function useGetList(){
+  const [list,setList] = useState([])
+
+  useEffect(()=>{
+    async function getList(){
+      const res = await fetch('https://fakestoreapi.com/products')
+      const resLists = await res.json()
+      setList(resLists)
+    }
+    getList()
+  },[])
+
+  return {list, setList}
+}
+
+
+//封裝列表組件
+function Items({item}){
+  return <li>{item.category}</li>
+}
+
 function App() {
+  const {list, setList} = useGetList()
+ 
   return (
     <div className="App">
-      this a APP
+      <ul>
+        {list.map(item=><Items key={item.id} item={item}/>)}
+      </ul>
     </div>
   );
 }
